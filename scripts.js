ba2c16607b796e9db4d1a590adf644e2ebd18709
Dayna-Ashley Roberts <daynaashley.roberts@mymona.uwi.edu>
2021-10-14 15:38:10 -0500

    Added Newsletter Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.getElementsByTagName("form")[0];
+    form.addEventListener("submit", (e) => {
+        
+       let message_area=document.querySelector('.message');
+       let email=document.querySelector('#email').value;
+       if (email==""){
+            message_area.innerHTML="Please enter a valid email address";
+            e.preventDefault();
+        
+        }
+        else{
+            message_area.innerHTML="Thank You! Your email address "+ email+ " has been added to our mailing list!";
+            e.preventDefault();
+        }
+
+        });
+
+});
